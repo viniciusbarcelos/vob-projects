5b26415789ef9487b63f692f0e92b7df7c34cbf3
viniciusbarcelos <barcelos.vinicius@gmail.com>
2015-07-12 00:36:30 -0300

    adicionando calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,20 @@
+package calc;
+ 
+public class Calculadora  {
+         
+    public float soma(float n1, float n2) {
+        return n1 + n2;
+    }
+     
+    public float subtracao(float n1, float n2) {
+        return n1 - n2;
+    }
+ 
+    public float multiplicacao(float n1, float n2) {
+        return n1 * n2;
+    }
+ 
+    public float divisao(float n1, float n2) {
+        return n1 / n2;
+    }
+}
